6b6b68bdbd383f14aa1ec33f7f5dfbf9c0755552
freitaslivia <xliviafreitasx@gmail.com>
2024-03-19 19:22:10 -0300

    api e dto

---
+++ src/main/java/br/com/fiap/jadv/rm99892/checkpoint/Rm99892CheckpointApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.jadv.rm99892.checkpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rm99892CheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rm99892CheckpointApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/fiap/jadv/rm99892/checkpoint/dto/CorridaDto.java
@@ -0,0 +1,5 @@
+package br.com.fiap.jadv.rm99892.checkpoint.dto;
+
+public record CorridaDto() {
+
+}
