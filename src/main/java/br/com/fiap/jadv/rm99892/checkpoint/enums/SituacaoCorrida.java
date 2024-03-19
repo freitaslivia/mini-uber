b7b3cc7da28806a85f154bb653f3786b6cd6368a
freitaslivia <xliviafreitasx@gmail.com>
2024-03-19 19:23:27 -0300

    enum e tests

---
+++ src/main/java/br/com/fiap/jadv/rm99892/checkpoint/enums/SituacaoCorrida.java
@@ -0,0 +1,8 @@
+package br.com.fiap.jadv.rm99892.checkpoint.enums;
+
+public enum SituacaoCorrida {
+	  AGUARDANDO,
+	  EM_ANDAMENTO,
+	  CONCLUIDA,
+	  CANCELADA
+}
---
+++ src/test/java/br/com/fiap/jadv/rm99892/checkpoint/Rm99892CheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.jadv.rm99892.checkpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rm99892CheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
