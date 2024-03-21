913e479daefc4a6cfae120795673bcc871553965
freitaslivia <126473298+freitaslivia@users.noreply.github.com>
2024-03-20 23:53:33 -0300

    Update CorridaException.java

--- src/main/java/br/com/fiap/jadv/rm99892/exception/CorridaException.java
+++ src/main/java/br/com/fiap/jadv/rm99892/exception/CorridaException.java
@@ -1,5 +1,12 @@
 package br.com.fiap.jadv.rm99892.exception;
 
-public class CorridaException {
+public class CorridaException extends RuntimeException{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public CorridaException(String message) {
+        super(message);
+    }
 }
