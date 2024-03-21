41fa7c002bac6ac3497db3da994eb59c15a2d176
freitaslivia <126473298+freitaslivia@users.noreply.github.com>
2024-03-20 23:48:21 -0300

    Update CorridaDto.java

--- src/main/java/br/com/fiap/jadv/rm99892/checkpoint/dto/CorridaDto.java
+++ src/main/java/br/com/fiap/jadv/rm99892/checkpoint/dto/CorridaDto.java
@@ -1,5 +1,18 @@
 package br.com.fiap.jadv.rm99892.checkpoint.dto;
 
-public record CorridaDto() {
+import java.sql.Timestamp;
 
+import br.com.fiap.jadv.rm99892.checkpoint.entidades.Corrida;
+import br.com.fiap.jadv.rm99892.checkpoint.enums.SituacaoCorrida;
+
+public record CorridaDto(Long id, String nomeCliente, String veiculoDs, String cpfCliente, String veiculoPlaca, String veiculoCor, Timestamp dtSolicitacao,
+		Timestamp dtFinalizacao,  double latitudeOrigem, double longitudeOrigem, double latitudeDestino,
+		double longitudeDestino, double latitudeAtual, double longitudeAtual,  SituacaoCorrida situacaoCorrida) {
+
+	public CorridaDto(Corrida corrida) {
+		this(corrida.getId(), corrida.getNomeCliente(), corrida.getVeiculoDs(), corrida.getCpfCliente(), corrida.getVeiculoPlaca(), corrida.getVeiculoCor(),
+				corrida.getDtSolicitacao(),  corrida.getDtFinalizacao(), corrida.getLatitudeOrigem(),
+				corrida.getLongitudeOrigem(), corrida.getLatitudeDestino(), corrida.getLongitudeDestino(),
+				corrida.getLatitudeAtual(), corrida.getLongitudeAtual(), corrida.getSituacaoCorrida());
+	}
 }
