0757f14a355bf045830de512ce25abeaf98327e5
freitaslivia <126473298+freitaslivia@users.noreply.github.com>
2024-03-20 23:51:43 -0300

    Update SituacaoCorrida.java

--- src/main/java/br/com/fiap/jadv/rm99892/checkpoint/enums/SituacaoCorrida.java
+++ src/main/java/br/com/fiap/jadv/rm99892/checkpoint/enums/SituacaoCorrida.java
@@ -1,8 +1,28 @@
 package br.com.fiap.jadv.rm99892.checkpoint.enums;
 
 public enum SituacaoCorrida {
-	  AGUARDANDO,
-	  EM_ANDAMENTO,
-	  CONCLUIDA,
-	  CANCELADA
+    AGUARDANDO("A"),
+    EM_ANDAMENTO("E"),
+    CONCLUIDA("C"),
+    CANCELADA("X");
+    
+    private String codigo;
+  
+  private SituacaoCorrida(String codigo) {
+      this.codigo = codigo;
+  }
+  
+  public String getCodigo() {
+      return codigo;
+  }
+  
+  public static SituacaoCorrida getCorridaPorCodigo(String codigo) {
+      for (SituacaoCorrida tipo : SituacaoCorrida.values()) {
+          if (tipo.getCodigo().equals(codigo)) {
+              return tipo;
+          }
+      }
+      return null;
+  }
+
 }
